612dd8fd1f0f44043fd87faba4d70ef998f687a4
Martin Böschen <boeschen@informatik.uni-bremen.de>
2010-07-06 16:56:38 +0200

    weiter am programm gebastelt

--- worstcase.m
+++ worstcase.m
@@ -1,24 +1,19 @@
-function wc=worstcase()
-
-r=0.05;
-mu=0.2;
-sigma=0.4;
-M=5000;
+function [w,g,wc]=worstcase(v,p)
+global mu r sigma k;
+M=500; % Anzahl der Simulation bei der Monte-Carlo-Simulation
 
 n=1000; % Anzahl der Schritte
 T=10; % Zeithorizont
 h=T/n; % Schrittweite
 
-p=2; (mu-r)/sigma^2;
-crashtimes=[1,2,3]
-% das zu den Crashzeiten gehörige Endvermögen
-ErwartetesEndvermoegen=zeros(1,length(crashtimes))
-t = [10,20,30,40,50,60,70,80,90]
-w=zeros(1,length(t));
+times=[0,2,5,7,10];
+time2step(times,10,n);
+
+w=zeros(1,length(times));
 
 
-% für alle crashzeiten
-for k=1:(length(t))
+% für alle crashzeiten den erwarteten Nutzen berechen
+for a=1:(length(times))
 	% Berechne mit einer Montecarlosimulation das erwartete Endvemögen bei crash in t
     % simultane Erzeugung der Brownschen Bewegung für m Pfade
 	dW=sqrt(h)*randn(n,M);
@@ -26,12 +21,34 @@ for k=1:(length(t))
 	X=zeros(1+n,M);
 	X(1,:)=1;
 	for j = 1:n
-		X(j+1,:)=X(j,:).*(1+(r+p*(mu-r))*h + p*sigma*dW(j,:));
+		X(j+1,:)=X(j,:).*(1+(r+p(step2time(j,T,n))*(mu-r))*h + p(step2time(j,T,n))*sigma*dW(j,:));
 	end
-    X;
+    % X(t(a),:)
+    % p(t(a))
+    % X(t(a),:)*(1-p(t(a))*k)
+    %
     %Berechnung des Schätzers
-    w(k)=(sum(v(h*t(k),X(t(k),:))))/M;
+    w(a)=(sum(v(times(a),X(time2step(times(a),T,n),:)*(1-p(times(a))*k))))/M;
 end
-w
+
+% den erwarteten Nutzen ohne Crash berechnen
+dW=sqrt(h)*randn(n,M);
+	% simultane Berechnung des Vermögens
+	X=zeros(1+n,M);
+	X(1,:)=1;
+	for j = 1:n
+		X(j+1,:)=X(j,:).*(1+(r+p(step2time(a,T,n))*(mu-r))*h + p(step2time(a,T,n))*sigma*dW(j,:));
+	end
+g=(sum(v(T,X(1000,:))))/M;
+
+% das Minimum von allen nehmen
+wc=min( [w,g]);
 endfunction
 
+function t=step2time(n,T,steps)
+t = n*T/steps;
+endfunction
+
+function n=time2step(t,T,steps)
+n = ceil(t*steps/T)+1;
+endfunction
